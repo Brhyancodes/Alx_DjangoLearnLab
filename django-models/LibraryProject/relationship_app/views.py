04cb6db98c61d2662c0d47daf626c4067bc13beb
Brian Wakhale <brianweke1998@gmail.com>
2025-09-07 09:49:52 +0300

    Fix:Fixed minor bug in views file for authetication

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Library, Book
 
 
