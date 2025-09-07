0b792ce6740dcaee47c65adca40360bf0323f471
Brian Wakhale <brianweke1998@gmail.com>
2025-09-07 09:34:51 +0300

    Fix:Final fix of import bug in urls file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
+from .views import list_books, LibraryDetailView
 from .views import (
-    list_books,
-    LibraryDetailView,
     RelationshipListView,
     RelationshipDetailView,
     RelationshipCreateView,
