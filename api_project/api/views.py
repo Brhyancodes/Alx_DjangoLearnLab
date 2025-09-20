7627ec3aa698620f50905ee329e76c7b9ba0d044
Brian Wakhale <brianweke1998@gmail.com>
2025-09-20 15:42:58 +0300

    feat(api): add Book model, serializer, and list endpoint with DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+#  create a view named BookList that extends rest_framework.generics.ListAPIView.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
