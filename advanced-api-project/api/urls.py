879ba1e6ba9de2401eb5e261c38e11e67f623a59
Brian Wakhale <brianweke1998@gmail.com>
2025-09-26 20:12:00 +0300

    Fix:Fix urls file in api directory

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,9 +8,11 @@ from .views import (
 )
 
 urlpatterns = [
-    path("books/", BookListView.as_view(), name="book-list"),
-    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),
-    path("books/create/", BookCreateView.as_view(), name="book-create"),
-    path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),
-    path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),
+    path("books/", BookListView.as_view(), name="book-list"),  # GET all
+    path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),  # GET one
+    path("books/", BookCreateView.as_view(), name="book-create"),  # POST (create)
+    path(
+        "books/<int:pk>/", BookUpdateView.as_view(), name="book-update"
+    ),  # PUT/PATCH (update)
+    path("books/<int:pk>/", BookDeleteView.as_view(), name="book-delete"),  # DELETE
 ]
