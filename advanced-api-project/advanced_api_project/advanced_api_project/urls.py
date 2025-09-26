fcfe729619d836f1d006dce0266ecb796ae4dccf
Brian Wakhale <brianweke1998@gmail.com>
2025-09-26 20:56:29 +0300

    Fix:Fix Urls file

--- advanced-api-project/advanced_api_project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/advanced_api_project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("api.urls")),  # <-- include app urls here
+    path("api/", include("api.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
