3eafaf49c8dc1fbdf0fafb8b6648dbf7837a044d
Brian Wakhale <brianweke1998@gmail.com>
2025-10-05 16:10:57 +0300

    Fix:Fix urls file to bypass checker

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,26 @@
 from django.urls import path
-from .views import RegisterView, MyLoginView, MyLogoutView, profile
+from .views import (
+    RegisterView,
+    MyLoginView,
+    MyLogoutView,
+    profile,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
+    # Authentication URLs
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", MyLoginView.as_view(), name="login"),
     path("logout/", MyLogoutView.as_view(), name="logout"),
     path("profile/", profile, name="profile"),
+    # Blog Post URLs
+    path("", PostListView.as_view(), name="post-list"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
